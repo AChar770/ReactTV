5d23eb2107b5f7f62114b4e8b070d2c656c84080
AChar770 <amandatcharles@yahoo.com>
2026-02-10 20:23:52 -0500

    1.  takes a  as a prop. 2.  keeps the selected episode in a state variable. 3. If no show  function is provided,  renders a message encouraging the user to select a show is completed

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,13 @@
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
+export default function ShowDetails({show}) {
   return <div className="show-details"></div>;
+
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  if (!show) {
+    return <p><strong>"Please choose a show to see more"</strong></p>;
 }
+
+return 
